38a726789c94814f8d3915e99e9b2157fb555656
Rebecca G. Bettencourt <beckiejo@gmail.com>
2020-05-11 00:28:23 -0700

    Tweak formatting of js output

--- java/DotBamCrak/src/MakeJS.java
+++ java/DotBamCrak/src/MakeJS.java
@@ -40,7 +40,9 @@ public class MakeJS {
 	
 	private static void makeTilesets(Library library, File root) throws IOException, ParseException {
 		PrintWriter out = open(new File(root, "tilesets.js"));
-		out.println("(window.DotBamCrak || (window.DotBamCrak = {})).tilesets = [");
+		out.println("(function(window){");
+		out.println("var dbc = window.DotBamCrak || (window.DotBamCrak = {});");
+		out.println("var ts = dbc.tilesets || (dbc.tilesets = []);");
 		for (TileSetInfo tilesetinfo : library.getTileSets()) {
 			TileSet tileset = tilesetinfo.createTileSet();
 			StringBuffer tsid = new StringBuffer();
@@ -48,35 +50,35 @@ public class MakeJS {
 			if (tsid.length() > 0) tsid.append("-");
 			tsid.append(tileset.getName().replaceAll("[^A-Za-z0-9]", "").toLowerCase());
 			
-			out.println("  {");
-			out.println("    id: " + quote(tsid.toString()) + ",");
-			out.println("    name: " + quote(tileset.getName()) + ",");
+			out.println("ts.push({");
+			out.println("  id: " + quote(tsid.toString()) + ",");
+			out.println("  name: " + quote(tileset.getName()) + ",");
 			if (tileset.getSourceName().length() > 0) {
-				out.println("    source: " + quote(tileset.getSourceName()) + ",");
-				out.println("    visible: false,");
+				out.println("  source: " + quote(tileset.getSourceName()) + ",");
+				out.println("  visible: false,");
 			} else {
-				out.println("    visible: true,");
+				out.println("  visible: true,");
 			}
 			
 			Dimension imageSize = tileset.getImageSize();
 			Dimension tileSize = tileset.getTileSize();
 			Insets insets = tileset.getImageInsets();
-			out.println("    dimensions: {");
-			out.println("      cell: [" + imageSize.width + ", " + imageSize.height + "],");
-			out.println("      tile: [" + tileSize.width + ", " + tileSize.height + "],");
-			out.println("      border: [" + insets.top + ", " + insets.right + ", " + insets.bottom + ", " + insets.left + "],");
-			out.println("    },");
+			out.println("  dimensions: {");
+			out.println("    cell: [" + imageSize.width + ", " + imageSize.height + "],");
+			out.println("    tile: [" + tileSize.width + ", " + tileSize.height + "],");
+			out.println("    border: [" + insets.top + ", " + insets.right + ", " + insets.bottom + ", " + insets.left + "],");
+			out.println("  },");
 			
-			out.println("    background: {");
+			out.println("  background: {");
 			if (tileset.getBackgroundImage() != null) {
 				String fn = "tilesets-" + tsid + "-bgimg.png";
 				RenderedImage bgimage = (RenderedImage)tileset.getBackgroundImage();
 				ImageIO.write(bgimage, "png", new File(root, fn));
-				out.println("      image: " + quote(fn) + ",");
+				out.println("    image: " + quote(fn) + ",");
 			}
 			if (tileset.getBackgroundPaint() instanceof Color) {
 				Color bgcolor = (Color)tileset.getBackgroundPaint();
-				out.println("      color: [" + bgcolor.getRed() + ", " + bgcolor.getGreen() + ", " + bgcolor.getBlue() + "],");
+				out.println("    color: [" + bgcolor.getRed() + ", " + bgcolor.getGreen() + ", " + bgcolor.getBlue() + "],");
 			} else {
 				String fn = "tilesets-" + tsid + "-bgpat.png";
 				BufferedImage bgimage = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
@@ -85,9 +87,9 @@ public class MakeJS {
 				g.fillRect(0, 0, 32, 32);
 				g.dispose();
 				ImageIO.write(bgimage, "png", new File(root, fn));
-				out.println("      pattern: " + quote(fn) + ",");
+				out.println("    pattern: " + quote(fn) + ",");
 			}
-			out.println("    },");
+			out.println("  },");
 			
 			String fn = "tilesets-" + tsid + ".png";
 			BufferedImage image = new BufferedImage(
@@ -101,61 +103,61 @@ public class MakeJS {
 			g.drawImage(blank.getImage(), 2, 2, null);
 			g.drawImage(blank.getSelectedImage(), 2, y, null);
 			int x = imageSize.width + 4;
-			out.println("    spritesheet: " + quote(fn) + ",");
-			out.println("    blank: {location: [2, 2, 2, " + y + "]},");
-			out.println("    tiles: {");
+			out.println("  spritesheet: " + quote(fn) + ",");
+			out.println("  blank: {location: [2, 2, 2, " + y + "]},");
+			out.println("  tiles: {");
 			List<String> tileIds = sorted(tileset.getTileMap().keySet());
 			for (String tileId : tileIds) {
 				Tile tile = tileset.getTile(tileId);
-				out.println("      " + quote(tile.getID()) + ": {");
-				out.println("        id: " + quote(tile.getID()) + ",");
+				out.println("    " + quote(tile.getID()) + ": {");
+				out.println("      id: " + quote(tile.getID()) + ",");
 				if (tile.getName().length() > 0) {
-					out.println("        name: " + quote(tile.getName()) + ",");
+					out.println("      name: " + quote(tile.getName()) + ",");
 				}
 				if (tile.getSuit() != null) {
-					out.println("        suit: " + quote(tile.getSuit().getID()) + ",");
+					out.println("      suit: " + quote(tile.getSuit().getID()) + ",");
 				}
 				if (tile.getRank() != null) {
-					out.println("        rank: " + quote(tile.getRank().getID()) + ",");
+					out.println("      rank: " + quote(tile.getRank().getID()) + ",");
 				}
 				if (tile.getHonorSet() != null) {
-					out.println("        honor: " + quote(tile.getHonorSet().getID()) + ",");
+					out.println("      honor: " + quote(tile.getHonorSet().getID()) + ",");
 				}
 				if (tile.getBonusSet() != null) {
-					out.println("        bonus: " + quote(tile.getBonusSet().getID()) + ",");
+					out.println("      bonus: " + quote(tile.getBonusSet().getID()) + ",");
 				}
 				if (tile.getMatchSet() != null) {
 					List<String> matches = new ArrayList<String>();
 					for (Tile t : tile.getMatchSet()) matches.add(t.getID());
-					out.println("        matches: " + quote(matches) + ",");
+					out.println("      matches: " + quote(matches) + ",");
 				}
-				out.println("        location: [" + x + ", 2, " + x + ", " + y + "],");
-				out.println("      },");
+				out.println("      location: [" + x + ", 2, " + x + ", " + y + "],");
+				out.println("    },");
 				g.drawImage(tile.getImage(), x, 2, null);
 				g.drawImage(tile.getSelectedImage(), x, y, null);
 				x += imageSize.width + 2;
 			}
 			g.dispose();
 			ImageIO.write(image, "png", new File(root, fn));
-			out.println("    },");
+			out.println("  },");
 			
-			printSetMap("    ", "suits", tilesetinfo.getSuitMap(), out);
-			printSetMap("    ", "ranks", tilesetinfo.getRankMap(), out);
-			printSetMap("    ", "honorSets", tilesetinfo.getHonorSetMap(), out);
-			printSetMap("    ", "bonusSets", tilesetinfo.getBonusSetMap(), out);
+			printSetMap("  ", "suits", tilesetinfo.getSuitMap(), out);
+			printSetMap("  ", "ranks", tilesetinfo.getRankMap(), out);
+			printSetMap("  ", "honorSets", tilesetinfo.getHonorSetMap(), out);
+			printSetMap("  ", "bonusSets", tilesetinfo.getBonusSetMap(), out);
 			
 			List<List<String>> matches = sorted2(tilesetinfo.getMatchSets());
 			if (!matches.isEmpty()) {
-				out.println("    matches: [");
+				out.println("  matches: [");
 				for (List<String> match : matches) {
-					out.println("      " + quote(match) + ",");
+					out.println("    " + quote(match) + ",");
 				}
-				out.println("    ],");
+				out.println("  ],");
 			}
 			
-			out.println("  },");
+			out.println("});");
 		}
-		out.println("];");
+		out.println("})(window);");
 		out.flush();
 		out.close();
 	}
@@ -173,13 +175,19 @@ public class MakeJS {
 	
 	private static void makeLayouts(Library library, File root) throws IOException {
 		PrintWriter out = open(new File(root, "layouts.js"));
-		out.println("(window.DotBamCrak || (window.DotBamCrak = {})).layouts = [");
+		out.println("(function(window){");
+		out.println("var dbc = window.DotBamCrak || (window.DotBamCrak = {});");
+		out.println("var lo = dbc.layouts || (dbc.layouts = []);");
 		for (Layout layout : library.getLayouts()) {
-			out.println("  { name: " + quote(layout.getName()) + ", visible: true, dimensions: {");
+			out.println("lo.push({");
+			out.println("  name: " + quote(layout.getName()) + ",");
+			out.println("  visible: true,");
+			out.println("  dimensions: {");
 			out.println("    board: [" + layout.getBoardWidth() + ", " + layout.getBoardHeight() + ", " + layout.getBoardDepth() + "],");
 			out.println("    tile: [" + layout.getTileWidth() + ", " + layout.getTileHeight() + ", " + layout.getTileDepth() + "],");
 			out.println("    window: [" + layout.getWindowWidth() + ", " + layout.getWindowHeight() + "],");
-			out.println("  }, tiles: [");
+			out.println("  },");
+			out.println("  tiles: [");
 			
 			List<String> tiles = new ArrayList<String>();
 			for (int z = 0, zn = layout.getBoardDepth(); z < zn; z++) {
@@ -197,25 +205,32 @@ public class MakeJS {
 				out.println(sb);
 			}
 			
-			out.println("  ]},");
+			out.println("  ],");
+			out.println("});");
 		}
-		out.println("];");
+		out.println("})(window);");
 		out.flush();
 		out.close();
 	}
 	
 	private static void makeFortunes(Library library, File root) throws IOException {
 		PrintWriter out = open(new File(root, "fortunes.js"));
-		out.println("(window.DotBamCrak || (window.DotBamCrak = {})).fortunes = [");
+		out.println("(function(window){");
+		out.println("var dbc = window.DotBamCrak || (window.DotBamCrak = {});");
+		out.println("var fs = dbc.fortunes || (dbc.fortunes = []);");
 		for (FortuneSet fortuneset : library.getFortuneSets()) {
 			String visible = (fortuneset.getName().equalsIgnoreCase("Fortunes")) ? "true" : "false";
-			out.println("  { name: " + quote(fortuneset.getName()) + ", visible: " + visible + ", fortunes: [");
+			out.println("fs.push({");
+			out.println("  name: " + quote(fortuneset.getName()) + ",");
+			out.println("  visible: " + visible + ",");
+			out.println("  fortunes: [");
 			for (String fortune : fortuneset) {
 				out.println("    " + quote(fortune) + ",");
 			}
-			out.println("  ]},");
+			out.println("  ],");
+			out.println("});");
 		}
-		out.println("];");
+		out.println("})(window);");
 		out.flush();
 		out.close();
 	}
